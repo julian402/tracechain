263d41eacb27301539693ce5ac3e322110bde312
Julian402 <corredor.jul@hotmail.com>
2026-06-01 20:20:44 -0500

    modify server

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from './src/app.js'
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port http://localhost:${PORT}`)
 })
